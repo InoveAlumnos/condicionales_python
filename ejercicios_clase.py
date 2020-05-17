71db98397ab898b78b826ab1494ac2f20e0dd188
InoveAlumnos <64550682+InoveAlumnos@users.noreply.github.com>
2020-05-17 14:41:08 -0300

    Add files via upload

---
+++ ejercicios_clase.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+'''
+Condicionales [Python]
+Ejercicios de clase
+---------------------------
+Autor: Inove Coding School
+Version: 1.1
+ 
+Descripcion:
+Programa creado para poner a prueba los conocimientos adquiridos durante la clase
+'''
+
+__author__ = "Inove Coding School"
+__email__ = "alumnos@inove.com.ar"
+__version__ = "1.1"
+
+def ej1():
+    # Ejercicios de práctica numérica
+    
+    # Comparadores
+    # Ingrese dos números cualesquiera y realice la sigueintes
+    # comparaciones entre ellos
+    print('Ingrese el primer número:')
+    numero_1 = int(input())
+
+    print('Ingrese el segundo número:')
+    numero_2 = int(input())
+    
+    # Compare cual de los dos números es mayor
+    # Imprima en pantalla según corresponda
+
+    # Verifique si el numero_1 positivo, negativo o cero
+    # Imprima el resultado en cada caso
+
+
+    # Verifique si el numero_1 es mayor a 0 y menor a 100
+    # Imprima en pantalla si se cumple o no la condición
+
+    # Verifique si el numero_1 es menor a 10 o el numero_2
+    # es mayor a -2
+    # Imprima en pantalla si se cumple o no la condición
+
+def ej2():
+    # Ejemplos variables de texto
+
+    # Comparadores
+    # Ingrese dos palabras cualesquiera y realice la sigueintes
+    # comparaciones entre ellas
+    print('Ingrese la primera palabra:')
+    texto_1 = str(input())
+
+    print('Ingrese la segunda palabra:')
+    texto_2 = str(input())
+
+    # Compare cual de las dos palabras es mayor
+    # Imprima en pantalla según corresponda
+
+    # Compare cual de las dos palabras tiene mayor
+    # cantidad de letras
+    # Imprima en pantalla según corresponda
+
+    # Verifique si la primera letra de la primera palabra
+    # es mayor a la primera letra de la segunda palabra
+    # Imprima en pantalla según corresponda
+
+    copia_texto_1 = texto_1  # Copia de la variable texto_1
+
+    # Verifique que copia_texto_1 es igual a texto_1
+    # Imprima en pantalla según corresponda
+
+    # Verifique que copia_texto_1 es distinta a texto_2
+    # Imprima en pantalla según corresponda
+
+def ej3():
+    # Ejercicios de práctica numérica
+
+    # Condicionales anidados
+    numero_1 = 7
+    numero_2 = -2
+
+    # Verifique si el numero_1 es mayor a 5
+    #   --> En caso afirmativo, verifique si el numero_2
+    #       es positivo
+    #       --> En caso afirmativo imprima en pantalla "Resp=1"
+    #       --> En caso negativo imprima en pantalla   "Resp=2"
+    #  --> En caso negativo (numero_1 no es mayor a 5) 
+    #      verifique si el numero_2 es mayor a 5
+    #       --> En caso afirmativo imprima en pantalla "Resp=3"
+    #       --> En caso negativo imprima en pantalla "Resp=4"
+
+    # Verifique la calificación de un estudiante según su
+    # puntaje en un examen
+    puntaje = 70
+
+    # Si el puntaje es mayor igual a 90 --> imprimir A
+    # Si el puntaje es mayor igual a 80 --> imprimir B
+    # Si el puntaje es mayor igual a 70 --> imprimir C
+    # Si el puntaje es mayor igual a 60 --> imprimir D
+    # Si el puntaje es manor a  60      --> imprimir F
+
+    # Debe imprimir en pantalla la calificacion
+    # Utilizar "if" anidados
+
+
+
+def ej4():
+    # Ejemplos variables de texto
+
+    texto_1 = '5'
+    texto_2 = '7'
+
+    # Verifique cual cual de los dos textos es mayor
+    # Imprima en pantalla según corresponda
+
+    # Transforma esas variables tipo texto y almacenalas
+    # en nuevas variables númericas (int)
+    # Repita el proceso, ¿Cuál de las nuevas variables es mayor?
+    # Imprima en pantalla según corresponda
+
+    # Para pensar!
+    # ¿Por qué cree que texto_2 es mayor a texto_1?
+    # Siendo números tiene sentido, pero son caracteres, texto
+    # aún así el operador arroja el mismo resultado que con las
+    # variables numéricas, cierto? ¿Por qué creen que es así?
+    # Esta pregunta estará repetida en el Campus para que puedan
+    # respondar.
+    # NOTA: La respuesta no se encuentra en el apunte, sino en Google ;)
+
+if __name__ == '__main__':
+    print("Bienvenidos a otra clase de Inove con Python")
+    #ej1()
+    #ej2()
+    #ej3()
+    #ej4()
+
---
+++ ejercicios_practica.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+'''
+Condicionales [Python]
+Ejercicios de práctica
+---------------------------
+Autor: Inove Coding School
+Version: 1.1
+ 
+Descripcion:
+Programa creado para que practiquen los conocimietos adquiridos durante la semana
+'''
+
+__author__ = "Inove Coding School"
+__email__ = "alumnos@inove.com.ar"
+__version__ = "1.1"
+
+def ej1():
+    # Ejercicios de práctica con números
+
+    '''
+    Realice un programa que solicite por consola 2 números
+    Calcule la diferencia entre ellos e informe por pantalla
+    si el resultado es positivo, negativo o cero.
+        
+    '''
+
+def ej2():
+# Ejercicios de práctica con números
+
+  '''
+    Realice un programa que solicite el ingreso de tres números
+    enteros, y luego en cada caso informe si el número es par
+    o impar.
+    Para cada caso imprimir el resultado en pantalla.
+    
+  '''
+
+def ej3():
+    # Ejercicios de práctica con números
+
+    '''
+    Realice una calculadora, se ingresará por línea de comando dos números
+    Luego se ingresará como tercera entrada al programa el símbolo de la operación
+    que se desea ejecutar
+    - Suma (+)
+    - Resta (-)
+    - Multiplicación (*)
+    - División (/)
+    - Exponente/Potencia (**)
+
+    Se debe efectuar el cálculo correcto según la operación ingresada por consola
+    Imprimir en pantalla la operación realizada y el resultado
+    
+
+    '''
+
+def ej4():
+    # Ejercicios de práctica con cadenas
+    
+    '''
+    Realice un programa que solicite por consola 3 palabras cualesquiera
+    Luego el programa debe consultar al usuario como quiere ordenar las palabras
+    1 - Ordenar por orden alfabético (usando el operador ">")
+    2 - Ordenar por cantidad de letras (longitud de la palabra)
+
+    Si se ingresa "1" por consola se deben ordenar las 3 palabras por orden alfabético
+    e imprimir en pantalla de la mayor a la menor
+
+    Si se ingresa "2" por consola se deben ordenar las 3 palabras por cantidad de letras
+    e imprimir en pantalla de la mayor a la menor
+
+  '''
+
+def ej5():
+    # Ejercicios de práctica con números
+       
+    '''
+    Realice un programa que solicite ingresar tres valores de temperatura
+    De las temperaturas ingresadas por consola determinar:
+    1 - ¿Cuáles de ellas es la máxima temperatura ingresada?
+    2 - ¿Cuáles de ellas es la mínima temperatura ingresada?
+    3 - ¿Cuáles es el promedio de las temperaturas ingresadas?
+
+    En cada caso imprimir en pantalla el resultado  
+
+    '''
+
+if __name__ == '__main__':
+    print("Ejercicios de práctica")
+    ej1()
+    #ej2()
+    #ej3()
+    #ej4()
+    #ej5()
