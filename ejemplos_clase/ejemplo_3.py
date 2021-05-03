e07a1c3f0a2d63b93f90d82c6d79b6695826e3e5
inoveAlumnos <alumnos@inove.com.ar>
2021-05-03 18:30:57 -0300

    Update ejemplos

--- ejemplos_clase/ejemplo_3.py
+++ ejemplos_clase/ejemplo_3.py
@@ -32,13 +32,4 @@ if len(texto_1) > len(texto_2):
 else:
     print('{} es mayor que {}'.format(texto_2, texto_1))
 
-
-# Condicionales compuestos
-# Si texto_1 es mayor a texto_2 e igual a "hola" o
-# texto_1 tiene menos de 4 letras, entonces imprimir "Correcto!"
-
-if (((texto_1 > texto_2) and texto_1 == 'hola') or
-    (len(texto_1) < 4)):
-    print('Correcto!')
-
 print("terminamos!")
