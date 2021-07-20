ea1b702c4d767b34a2c95a252796612b89329c05
InoveAlumnos <64550682+InoveAlumnos@users.noreply.github.com>
2021-07-20 14:18:39 -0300

    Update profundizacion_4.py

--- ejercicios_profunidzacion/profundizacion_4.py
+++ ejercicios_profunidzacion/profundizacion_4.py
@@ -18,13 +18,17 @@ Enunciado:
 Realice un programa que solicite por consola 3 palabras cualesquiera
 Luego el programa debe consultar al usuario como quiere ordenar las palabras
 1 - Ordenar por orden alfabético (usando el operador ">")
-2 - Ordenar por cantidad de letras (longitud de la palabra)
+2 - Ordenar por cantidad de letras (longitud de la palabra (len) y operador ">")
 
 Si se ingresa "1" por consola se deben ordenar las 3 palabras por orden alfabético
 e imprimir en pantalla de la mayor a la menor
 
 Si se ingresa "2" por consola se deben ordenar las 3 palabras por cantidad de letras
 e imprimir en pantalla de la mayor a la menor
+
+IMPORTANTE: Para ordenar las palabras deben realizar condicionales compuestos o anidados,
+no se busca utilizar bucles o algoritmos de ordenamiento ya que aún no hemos llegado a ese
+contenido.
 '''
 
 print('Ejercicios de práctica con cadenas')
